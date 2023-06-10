bddf7ebcf72804beecfa59b2bae75f98deb37fae
SeungHyeon Yoo <shyeon.yoo@gmail.com>
2023-06-11 02:30:28 +0900

    Update Clustering.scala

--- src/main/scala/clustering/Clustering.scala
+++ src/main/scala/clustering/Clustering.scala
@@ -1,16 +1,13 @@
 //2022.04.20 created by ysh 
 
-import scala.io.Source
-import org.apache.spark.rdd.RDD
-import org.apache.hadoop.io.Text
-import org.apache.hadoop.io.IntWritable
-import scala.util.control._
-import org.apache.spark.{SparkConf, SparkContext}
+package com.example.clustering
+
+/**
+ * @author ${user.name}
+ */
 import org.apache.spark.rdd.RDD // RDD 형식 파일
-import org.apache.hadoop.fs.{FileSystem, Path, FileStatus} //hadoop FileSystem API
 import org.apache.spark._
-import org.apache.hadoop.fs._
-import org.apache.spark.sql.{Row,SparkSession,SQLContext,DataFrame}
+import org.apache.spark.sql.{Row,SparkSession,SQLContext,DataFrame,Encoders}
 import org.apache.spark.sql.functions.lit
 import org.apache.log4j.{Level, LogManager, Logger}
 import org.apache.spark.sql.types.{DataTypes,StructType, StructField, StringType, IntegerType, FloatType, MapType, DoubleType, ArrayType}
@@ -32,83 +29,95 @@ import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.functions._
 import org.apache.spark.ml.stat.Summarizer
 import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
-import breeze.linalg.DenseMatrix
 import org.apache.spark.mllib.recommendation.Rating
 
-object Clustering {
-    def main(args: Array[String]) : Unit = {
-         val spark:SparkSession = SparkSession.builder()
-                                                .master("yarn")
-                                                .appName("rbf_cluster")
-                                                .getOrCreate()
-        val sc = spark.sparkContext
-        //val sqlContext = new org.apache.spark.sql.SQLContext(sc) //데이터 프레임
-        //sqlContext.setConf("spark.sql.shuffle.partitions","40")
-        //import sqlContext.implicits._ // 데이터 프레임 라이브러리       
+object App {
+  case class ImageFeature(id: Long, approxFeatures: Seq[Vector], actualFeatures: Vector)
 
-        val feature_path_query = "/wedrive_data/raw_data/query_row.csv" // 이미지 벡터 파일 
-        //val feature_path_whole = "/wedrive_data/raw_data/feature_row.csv"// 만 개 이미지 벡터 파일 
-        val feature_path_whole = "/wedrive_data/raw_data/millon.csv"// 백만 개 이미지 벡터 파일 
-        val img_list_query = data_process(feature_path_query, sc).flatMap(_._2)
-        val img_list_whole = data_process(feature_path_whole, sc)
-
-        val point = 1000
-        //val list = img_list_query.collect.map(x => ((x * point).toInt))
-        val list = img_list_query.collect.map(x => x)
-        //시간 측정
-        var start = System.nanoTime() //나노 초로 시간 측정 
-        
-        val res = img_list_whole.map { line => 
-        //val data = line._2.map(x => ((x * point).toInt))
-        val data = line._2.map(x => x)
-        val data_res = rbf_kernel(data, list)
-        (line._1 , data_res)
-        } 
-        
-        println("\n\n")
-        var end = System.nanoTime()  // 나노 초로 끝 시간 측
-        res.sortBy(_._2).take(10).foreach(println) 
-        println("\n\n")
-        println("==========================================================================================")
-        println("                                      RBF Kernel                                          ")
-        println("==========================================================================================")     
-        println("Completed")
-        println("\n\n")
-        println("==========================================================================================")
-        println("                                      Time Taken                                          ")
-        println("==========================================================================================")
-        println(s"Time Taken: ${NANOSECONDS.toNanos(end-start)}ns")
-        println("\n\n")
-        sc.stop()
-    }
-
-    def data_process (feature_file : String, sc : SparkContext) : RDD[(String , Array[Float])] = {   
-       
-        val slength = 4096
-
-        val feature_RDD = sc.textFile(feature_file, minPartitions=8) //RDD생성
-
-        val list = feature_RDD.map { x => 
-        val spl = x.replace("\\[|\\]","")
-                   .replace("(","")
-                   .replace("))","")
-                   .replace("Vector","").split(",")
-        val key = spl(0)
-        val fvals = spl.slice(1,slength).map(x => x.toFloat)    
-        (key,fvals)
-        }
-        return list
-    }
-
-    def rbf_kernel(fv1 : Array[Float], fv2 : Array[Float]) = {
-    // 0.1 , 0.5 , 10
+  def approximateFeatures(features: Vector): Seq[Vector] = {
+    // 특징 벡터를 이용하여 근사화된 벡터를 생성
+    val approxFeature = Vectors.dense(features.toArray.map {
+      case value if value >= 0.0 && value <= 0.4 => 0.0
+      case value if value > 0.4 && value <= 0.9 => 1.0
+      case value => value
+    })
+    // 생성된 근사화된 벡터를 반환
+    Seq(approxFeature)
+  }
+  // 유사도 계산 함수
+  def compareFeatures(imageFeatures: Vector, queryFeatures: Vector): Double = {
     val gamma = 0.5
-    val sum = fv1.zip(fv2).map {
-            case (p1, p2) => 
-                val d = p2 - p1
-                scala.math.pow(d,2)
-            }.sum 
-    val res = scala.math.exp(-gamma / sum)
-    res
+    val sum = imageFeatures.toArray.zip(queryFeatures.toArray).map {
+      case (p1, p2) =>
+        val d = p2 - p1
+        math.pow(d, 2)
+    }.sum
+    math.exp(-gamma / sum)
+  }
+
+  def main(args: Array[String]): Unit = {
+    // Spark 세션 생성
+    val spark = SparkSession.builder()
+      .appName("SimilaritySearch")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    // 이미지 특징 벡터 데이터
+     // 이미지 특징 벡터 데이터를 CSV 파일에서 로드합니다.
+    val rawDataPath = "path/to/your/raw_data.csv"
+    val rawDataRDD = spark.sparkContext.textFile(rawDataPath).map { line =>
+      val tokens = line.split(',')
+      (tokens(0).toLong, Vectors.parse(tokens(1)))
     }
+    val rawData = rawDataRDD.toDF("id", "features")
+
+   // 2단계 검색에서 1단계 이산화된 코드 전의 데이터
+    val imageFeatures = rawData.as[(Long, Vector)].map {
+      case (id, features) =>
+        val approxFeatures = approximateFeatures(features)
+        ImageFeature(id, approxFeatures, features)
+    }
+
+    // 비교하고자 하는 이미지 특징 벡터를 CSV 파일에서 로드합니다.
+    val queryFeaturesPath = "path/to/your/query_features.csv"
+    val queryFeaturesString = spark.sparkContext.textFile(queryFeaturesPath).first()
+    val queryFeatures = Vectors.parse(queryFeaturesString)
+
+
+    val k = 3 // 근접 이웃 개수
+    var start = System.nanoTime() // 시작 시간 측정
+
+    // 유사도 계산 후 결과를 DataFrame으로 저장
+    val similarities = imageFeatures.map {
+      case ImageFeature(id, features, _) =>
+        val similarity = features.map(approx => compareFeatures(approx, queryFeatures)).max
+        (id, similarity)
+    }.toDF("id", "similarity")
+
+    var end = System.nanoTime() // 종료 시간 측정
+
+    // 유사도 기준 상위 k개의 결과 선택
+    val topKImages = similarities.orderBy($"similarity".desc).take(k)
+
+    val duration = (end - start) // 소요 시간 계산 (초 단위)
+    println("\n\n")
+    println("==========================================================================================")
+    println("                                      similarities                                        ")
+    println("==========================================================================================")
+    // 결과 출력
+    topKImages.foreach { row =>
+      println(s"ID: ${row.getAs[Long]("id")}, Similarity: ${row.getAs[Double]("similarity")}")
+    }  
+    println("Completed")
+    println("\n\n")
+    println("==========================================================================================")
+    println("                                      Time Taken                                          ")
+    println("==========================================================================================")
+    println(s"Time Taken: ${NANOSECONDS.toNanos(end-start)}ns")
+    println("\n\n")
+
+    // Spark 종료
+    spark.stop()
+  }
 }
